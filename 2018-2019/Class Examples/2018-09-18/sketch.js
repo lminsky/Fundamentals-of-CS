f81be4b4fbaa7a32374b1f435e95d6b45349a52e
Louis Minsky <lminsky@packer.edu>
2018-09-18 08:38:58 -0400

    added shapes

---
+++ 2018-2019/Class Examples/2018-09-18/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(700, 700);
+  background(0, 255, 255);
+  ellipse(50, 50, 25, 10);
+  rect(50, 50, 75, 40);
+  triangle(20, 50, 30, 30, 30, 75);
+}
+
+function draw() {
+  // put drawing code here
+}
