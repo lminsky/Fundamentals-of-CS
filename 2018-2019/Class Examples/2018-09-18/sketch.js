66c866b8ec07f091fe0ba9e0c19dc6a0f866b854
Louis Minsky <lminsky@packer.edu>
2018-09-18 08:42:06 -0400

    reset background color

--- 2018-2019/Class Examples/2018-09-18/sketch.js
+++ 2018-2019/Class Examples/2018-09-18/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   // put setup code here
   createCanvas(700, 700);
-  background(5, 255, 255);
+  background(0, 255, 255);
   ellipse(50, 50, 25, 10);
   rect(50, 50, 75, 40);
   triangle(20, 50, 30, 30, 30, 75);
